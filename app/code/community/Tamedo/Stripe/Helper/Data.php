bff79b8a68fdcd70938f52bf0c9391eabd4c1eef
Daniel Rafique <rafique@tamedo.com>
2014-03-29 11:26:23 +0000

    Final commit following more tests to remove bugs...

--- app/code/community/Tamedo/Stripe/Helper/Data.php
+++ app/code/community/Tamedo/Stripe/Helper/Data.php
@@ -6,7 +6,6 @@
  * @package		Tamedo_Stripe
  * @author		Daniel Rafique <rafique@tamedo.com>
  * @copyright	Tamedo (http://tamedo.com)
- * @license		http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class Tamedo_Stripe_Helper_Data extends Mage_Core_Helper_Abstract
 {
--- app/code/community/Tamedo/Stripe/Model/Source/Cctype.php
+++ app/code/community/Tamedo/Stripe/Model/Source/Cctype.php
@@ -6,7 +6,6 @@
  * @package		Tamedo_Stripe
  * @author		Daniel Rafique <rafique@tamedo.com>
  * @copyright	Tamedo (http://tamedo.com)
- * @license		http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class Tamedo_Stripe_Model_Source_Cctype extends Mage_Payment_Model_Source_Cctype
 {
